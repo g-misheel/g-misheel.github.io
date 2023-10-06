982b34ca7857b0b07bf164819d7623273965c5ad
g-misheel <97901209+g-misheel@users.noreply.github.com>
2023-10-06 10:38:45 -0600

    factored the ball

--- 03-ball-object/sketch.js
+++ 03-ball-object/sketch.js
@@ -1,19 +1,12 @@
 // "Ball Object Notation" Demo
 // Oct 5, 2023
 
-let theBall = {
-  x: 100,
-  y: 100,
-  radius: 25,
-  r: 255,
-  g: 0,
-  b: 0,
-  dx: -4,
-  dy: 3,
-};
+
+let theBall;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theBall = spawnBall();
 }
 
 function draw() {
@@ -22,13 +15,27 @@ function draw() {
   displayBall();
 }
 
-
-function displayBall() {
-  fill(theBall.r, theBall.g, theBall.b);
-  circle(theBall.x, theBall.y, theBall.radius * 2);
+function keyTyped() {
+  if (key === " ") {
+    theBall = spawnBall();
+  }
 }
 
-function moveBall(){
+function spawnBall() {
+  let theBall = {
+    x: random(width),
+    y: random(height),
+    radius: random(15, 30),
+    r: random(255),
+    g: random(255), 
+    b: random(255),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+  };
+  return theBall;
+}
+
+function moveBall() {
   theBall.x += theBall.dx;
   theBall.y += theBall.dy;
 
@@ -38,18 +45,24 @@ function moveBall(){
   }
 
   // off the left side
-  if (theBall.x < 0 - theBall.radius){
+  else if (theBall.x < 0 - theBall.radius) {
     theBall.x = width + theBall.radius;
   }
-  
-  // off the  bottom
-  else if (theBall.y - theBall.radius > height) {
+
+  // off the bottom
+  if (theBall.y - theBall.radius > height) {
     theBall.y = 0 - theBall.radius;
   }
 
   // off the top
-  if (theBall.y < 0 - theBall.radius){
+  else if (theBall.y < 0 - theBall.radius) {
     theBall.y = height + theBall.radius;
   }
-  
+
+  console.log(theBall.x, theBall.y);
+}
+
+function displayBall() {
+  fill(theBall.r, theBall.g, theBall.b);
+  circle(theBall.x, theBall.y, theBall.radius * 2);
 }
