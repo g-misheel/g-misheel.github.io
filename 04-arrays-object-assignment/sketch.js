49a385f40c5438e15e04ab084bf074cb5dee9c47
g-misheel <97901209+g-misheel@users.noreply.github.com>
2023-10-10 11:55:55 -0600

    arrays-object-assignment created

---
+++ 04-arrays-object-assignment/sketch.js
@@ -0,0 +1,15 @@
+// Arrays Object Notation Assignment
+// Misheel Gankhuyag
+// October 10, 2023
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
--- index.md
+++ index.md
@@ -7,3 +7,4 @@
 
 ## Projects
 - [Interactive Scene](02-scene)
+- [ Arrays and Object Notation Assignment](04-arrays-object-assignment)
