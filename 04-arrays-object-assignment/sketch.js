b372dbb4a852c6b5cf08f93780ebb04fbd10e34b
g-misheel <mishkagankhuyag@gmail.com>
2023-10-18 21:24:17 -0600

    added score, level, function that removes the blocks when the 'tower' is too tall

--- 04-arrays-object-assignment/sketch.js
+++ 04-arrays-object-assignment/sketch.js
@@ -6,21 +6,23 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let state = "start screen";  // The current game state (start screen or game)
-let leftSide;  
-let topSide;  
-let boxWidth;  
+let state = "start screen";
+let leftSide;
+let topSide;
+let boxWidth;
 let boxHeight;
 
 let tower = [];
 let blockHeight = 40;
 let currentBlock;
-let blockWidth = 150;
-
 let fundamentalBlock;
 
 let gameOver;
 let gameOverImg;
+
+let score = 0;
+let level = 1;
+let fallingSpeed = 5;
 
 function preload() {
   // Load images and store them in variables
@@ -31,10 +33,10 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
 
-  boxWidth = 300;  // Width of the start button rectangle
-  boxHeight = 50;  // Height of the start button rectangle
-  leftSide = (windowWidth - boxWidth) / 2;  // Centered horizontally
-  topSide = (windowHeight - boxHeight) / 2;  // Centered vertically
+  boxWidth = 300;
+  boxHeight = 50;
+  leftSide = (windowWidth - boxWidth) / 2;
+  topSide = (windowHeight - boxHeight) / 2;
   initializeGame();
 
   gameOver = false;
@@ -42,131 +44,125 @@ function setup() {
 
 function draw() {
   if (state === "start screen") {
-    startScreen();  // Display the start screen
-  } 
-
-  //Start game
-  else if (state === "game") {
+    startScreen();
+  } else if (state === "game") {
     background("lightblue");
-    
+
     if (!gameOver) {
       drawTower();
       drawCurrentBlock();
-
+      drawScoreAndLevel();
 
       if (currentBlock) {
-        currentBlock.y += 5; // Falling speed
+        currentBlock.y += fallingSpeed;
+
         if (blockTouchesFundamentalBlock(currentBlock)) {
-          // If the block touches the fundamental block, it's added to the tower.
           addToTower(currentBlock);
           initializeNewBlock();
-        }
-        else if (blockHitsGround(currentBlock)) {
-          // If the block touches the ground without landing on the fundamental block, the game is over.
+          score++;
+          if (score % 5 === 0) {
+            removeAndPushBlocks();
+            level++;
+            fallingSpeed += 1;
+          }
+        } else if (blockHitsGround(currentBlock)) {
           showGameOver();
         }
       }
 
-
       if (currentBlock) {
         if (keyIsDown(65)) {
-          // A key (move left)
           if (currentBlock.x - 5 >= 0) {
             currentBlock.x -= 5;
           }
         }
         if (keyIsDown(68)) {
-          // D key (move right)
-          if (currentBlock.x + blockWidth + 5 <= windowWidth) {
+          if (currentBlock.x + currentBlock.width + 5 <= windowWidth) {
             currentBlock.x += 5;
           }
         }
       }
     }
-  }
-
-  else {
-    showGameOver();  // Display the game over screen
+  } else {
+    showGameOver();
   }
 }
 
-
 function initializeGame() {
   tower = [];
+  score = 0;
+  level = 1;
+  fallingSpeed = 5;
   initializeFundamentalBlock();
   initializeNewBlock();
 }
 
+function removeAndPushBlocks() {
+  if (tower.length >= 5) {
+    tower.shift();
+  }
+}
 
 function initializeFundamentalBlock() {
   fundamentalBlock = {
-    x: windowWidth / 2 - blockWidth / 2,
-    color: color(0, 0, 0), // Fundamental block is black
+    x: windowWidth / 2 - blockHeight / 2,
+    color: color(0, 0, 0),
+    width: 150,  // Set a fixed width for the fundamental block
   };
 }
-
 
 function initializeNewBlock() {
   currentBlock = {
-    x: random(windowWidth/4, windowWidth - windowWidth/4),
+    x: random(windowWidth / 4, windowWidth - windowWidth / 4),
     y: 0,
     color: color(random(255), random(255), random(255)),
+    width: random(100, 200),  // Random width between 100 and 200
   };
 }
-
 
 function drawTower() {
   for (let i = 0; i < tower.length; i++) {
     let block = tower[i];
     fill(block.color);
-    rect(block.x, block.y, blockWidth, blockHeight);
+    rect(block.x, block.y, block.width, blockHeight);
   }
 
-
   fill(fundamentalBlock.color);
-  rect(fundamentalBlock.x, windowHeight - blockHeight, blockWidth, blockHeight);
+  rect(fundamentalBlock.x, windowHeight - (blockHeight + 5), fundamentalBlock.width, blockHeight);
 }
-
 
 function drawCurrentBlock() {
   if (currentBlock) {
     fill(currentBlock.color);
-    rect(currentBlock.x, currentBlock.y, blockWidth, blockHeight);
+    rect(currentBlock.x, currentBlock.y, currentBlock.width, blockHeight);
   }
 }
-
 
 function blockTouchesFundamentalBlock(block) {
   let fundamentalTopY = windowHeight - blockHeight;
   return (
-    block.x + blockWidth > fundamentalBlock.x &&
-    block.x < fundamentalBlock.x + blockWidth &&
+    block.x + block.width > fundamentalBlock.x &&
+    block.x < fundamentalBlock.x + fundamentalBlock.width &&
     block.y + blockHeight === fundamentalTopY
   );
 }
 
-
 function blockHitsGround(block) {
   return block.y + blockHeight >= windowHeight;
 }
-
 
 function addToTower(block) {
   block.y = windowHeight - blockHeight * (tower.length + 1);
   tower.push(block);
 }
 
-
 function keyPressed() {
   if (keyCode === 32) {
-    // Space key to start a new game
     initializeGame();
     loop();
   }
 }
 
-
-// Display the start screen
 function startScreen() {
   noStroke();
   fill("lightgreen");
@@ -174,10 +170,9 @@ function startScreen() {
 
   fill("black");
   textSize(24);
-  text("Click to Start", leftSide + boxWidth / 4, topSide + boxHeight / 2 + 10); // Centered text
+  text("Click to Start", leftSide + boxWidth / 4, topSide + boxHeight / 2 + 10);
 }
 
-// Handle mouse click to start the game
 function mousePressed() {
   if (state === "start screen") {
     let isClicked = isInRect(mouseX, mouseY, topSide, topSide + boxHeight, leftSide, leftSide + boxWidth);
@@ -187,15 +182,21 @@ function mousePressed() {
   }
 }
 
-// Check if a point is inside the 'start' button
 function isInRect(x, y, top, bottom, left, right) {
   return x >= left && x <= right && y >= top && y <= bottom;
 }
 
-//Game over screen
 function showGameOver() {
   noLoop();
-  image(gameOverImg, windowWidth / 2, windowHeight / 2, gameOverImg.width , gameOverImg.height);
+  image(gameOverImg, windowWidth / 2, windowHeight / 2, gameOverImg.width, gameOverImg.height);
   console.log("Game Over");
+}
+
+function drawScoreAndLevel() {
+  fill(0);
+  textSize(18);
+  textAlign(RIGHT);
+  text(`Score: ${score}`, windowWidth - 20, 30);
+  text(`Level: ${level}`, windowWidth - 20, 60);
 }
 
