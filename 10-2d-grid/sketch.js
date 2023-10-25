1cd77f1128468ed46920c6261b2a6d8c32ae791a
g-misheel <97901209+g-misheel@users.noreply.github.com>
2023-10-25 11:30:37 -0600

    added mousepressed function

--- 10-2d-grid/sketch.js
+++ 10-2d-grid/sketch.js
@@ -2,11 +2,14 @@
 // Misheel Gankhuyag
 // October 24
 
-// let grid = [[1, 0, 0, 1], [0, 0, 1, 1], [1, 1, 0, 1], [0, 1, 1, 1]];
+// let grid = [[1, 0, 0, 1], 
+//            [0, 0, 1, 1], 
+//            [1, 1, 0, 1], 
+//            [0, 1, 1, 1]];
 
 let grid;
 let cellSize;
-const GRID_SIZE = 15;
+const GRID_SIZE = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -23,6 +26,19 @@ function setup() {
 function draw() {
   background(220);
   displayGrid();
+}
+
+function mousePressed() {
+  let y = Math.floor(mouseY/cellSize);
+  let x = Math.floor(mouseX/cellSize);
+
+  if (grid[y][x] === 0) {
+    grid[y][x] = 1;
+  }
+  else if (grid[y][x] === 1) {
+    grid[y][x] = 0;
+  }
+
 }
 
 function keyTyped(){
